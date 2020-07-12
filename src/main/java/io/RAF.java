816b73c89f3b0d532454fe0496091246eebe304b
Mehdi <mehditeymorian322@gmail.com>
2020-07-12 23:49:40 +0430

    complete RAF.getDynamicAddress()

--- src/main/java/io/RAF.java
+++ src/main/java/io/RAF.java
@@ -3,19 +3,24 @@ package main.java.io;
 import lombok.Getter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
-public abstract class RAF<T> implements AutoCloseable{
-    public static final String BASE_ADDRESS = "main/resources/";
-    private File file;
+public abstract class RAF<T> implements AutoCloseable {
+    public static final String BASE_ADDRESS = "/src/main/resources/";
 
     @Getter
     private RandomAccessFile raf;
 
     public RAF(String address) {
         String dynamicAddress = RAF.getDynamicAddress(address);
-        // TODO: 6/18/2020 create file and raf
+        try {
+            raf = new RandomAccessFile(dynamicAddress , "rw");
+        } catch (FileNotFoundException e) {
+            System.err.println("Failed to create RandomAccessFile for address '" + dynamicAddress + "'");
+            e.printStackTrace();
+        }
     }
 
     public abstract void add(T obj) throws IOException;
@@ -25,9 +30,17 @@ public abstract class RAF<T> implements AutoCloseable{
         return getRaf().length();
     }
 
-    public static String getDynamicAddress(String file) {
-        // TODO: 6/18/2020 complete this
-        return null;
+    public static String getDynamicAddress(String fileAddress) {
+        File file = new File(BASE_ADDRESS + fileAddress);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                System.err.println("Failed to create file with address: " + fileAddress);
+                e.printStackTrace();
+            }
+        }
+        return file.getPath();
     }
 
     @Override
