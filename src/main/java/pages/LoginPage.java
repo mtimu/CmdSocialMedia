96cd829ee646fd18f9bea00ccfaac4e98883aab8
Mehdi <mehditeymorian322@gmail.com>
2020-07-16 20:05:26 +0430

    Feature: Add Sign-up to Login flow
    Feature:
    Add Login Menu and A flow consist of Sign-up, Login and Exit.
    
    Refactor:
    Refactor user authentication

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,26 +1,43 @@
 package main.java.pages;
 
-import main.java.ui.Printer;
 import main.java.account.Credentials;
-import main.java.model.User;
+import main.java.ui.Menu;
+import main.java.ui.Printer;
 
 public class LoginPage extends Page {
+    public static final String TITLE = "Login Menu";
+    public static final String[] MENU = {"Add User" , "Login" , "EXIT"};
+    public static final int ADD_USER = 1;
+    public static final int LOGIN = 2;
 
 
     @Override
     public void onStart() {
         Printer.println("Welcome, Sharing is caring.");
-        Printer.println("Enter your Credentials:");
-        auth();
+        Menu.printLoginMenu();
+        int choice = getInput().intIn();
+        switch (choice) {
+            case ADD_USER:
+                getPageManager().addToStack(AddUserPage.class);
+                break;
+
+            case LOGIN:
+                auth();
+                break;
+
+            default:
+                // EXIT
+        }
     }
 
     private void auth() {
+        Printer.println("Enter your Credentials:");
         String username = getInput().lineIn("Username").trim();
         String password = getInput().lineIn("Password").trim();
 
-        User user = Credentials.getInstance().authenticate(username , password);
+        boolean authenticate = Credentials.getInstance().authenticate(username , password);
 
-        if (user != null) {
+        if (authenticate) {
             getPageManager().addToStack(MainPage.class);
             return;
         }
