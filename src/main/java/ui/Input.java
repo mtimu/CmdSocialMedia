0f5bfdbed549621d089ef078e4549abeb0ccf7f3
Mehdi <mehditeymorian322@gmail.com>
2020-07-18 11:38:58 +0430

    REFACTOR: Remove strIn() and strIn(String) methods

--- src/main/java/ui/Input.java
+++ src/main/java/ui/Input.java
@@ -41,16 +41,6 @@ public class Input {
         return scanner.nextLine().trim();
     }
 
-    public String strIn() {
-        return strIn(TITLE);
-    }
-
-    // TODO: 7/14/2020 fix this shit
-    public String strIn(String title) {
-        Printer.print(title + ": " , Printer.COLOR_GREEN);
-        String val = scanner.nextLine().trim();
-        return val;
-    }
 
     public void pressEnterToContinue() {
         Printer.print("Press Enter To Continue...");
